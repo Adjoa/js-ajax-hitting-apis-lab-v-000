9172a3412dd60ce34b4e283df69f1bb42a158e43
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-11 18:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -47,8 +47,8 @@ function getCommits(el) {
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
+  
   document.getElementById("repositories").innerHTML = repoList
 }
 
