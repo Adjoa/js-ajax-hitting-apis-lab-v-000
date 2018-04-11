64b045315d8090ae34b5d73a453eb5dd488a0279
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-11 18:32:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ function displayCommits() {
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo
+  const name = el.dataset.repository
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayCommits)
   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
