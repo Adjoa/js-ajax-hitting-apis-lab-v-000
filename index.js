025543a8109756ec5cf7195c01ba46efb3499484
Adjoa Darien <adjoa.darien@gmail.com>
2018-04-11 18:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// function displayCommits() {
-//   const commits = JSON.parse(this.responseText)
-//   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-//   document.getElementById("details").innerHTML = commitsList
-// }
-//
-// function getCommits(el) {
-//   const username = el.dataset.username
-//   const repo = el.dataset.repo
-//   const req = new XMLHttpRequest()
-//   req.addEventListener("load", showCommits)
-//   req.open("GET", 'https://api.github.com/repos/' + username+ '/' + repo + '/commits')
-//   req.send()
-// }
-//
-// function showRepositories(event, data) {
-//   var repos = JSON.parse(this.responseText)
-//   console.log(repos)
-//   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + 'data-username="' + r.owner.login + '"  onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
-//
-//   document.getElementById("repositories").innerHTML = repoList
-// }
-//
-//
-// function getRepositories() {
-//   let username = document.getElementById('username').value
-//   const req = newXMLHttpRequest()
-//   req.addEventListener("load", showRepositories)
-//   req.open("GET", 'https://api.github.com/users/' + username + '/repos')
-//   req.send()
-// }
 function displayBranches(){
   const branches = JSON.parse(this.responseText)
   const branchesList = `<ul>${branches.map(branch => '<li><strong>' + branch.name + '</strong> ' + '</li>').join('')}</ul>`
